9333c29c0926de80c9e4800a01f42ead6789a1cd
Aditya Singh <adityasingh8532@gmail.com>
2024-07-06 21:20:00 +0200

    Add token to axios requests

--- frontend/src/core/Cards.js
+++ frontend/src/core/Cards.js
@@ -3,7 +3,7 @@ import { useParams, Link } from 'react-router-dom';
 import axios from 'axios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrashAlt, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
-import { deleteCardPrompt } from './Common';
+import { deleteCardPrompt, setAxiosDefaults } from './Utils';
 import '../Common.css';
 
 
@@ -15,7 +15,7 @@ export function Cards() {
     const [newCardBack, setNewCardBack] = useState('');
 
     const API_URL = '/api';
-
+    setAxiosDefaults();
 
     const fetchCards = async () => {
         const response = await axios.get(`${API_URL}/category/${id}`);
